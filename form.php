32a6fbdd23d89767ff6812992ba1cbf2e755491f
francescogentile25 <francescogentile250893@gmail.com>
2023-03-17 16:20:48 +0100

    FEAT add censured word

--- form.php
+++ form.php
@@ -1,4 +1,7 @@
 <?php
-$word = $_GET['word']
+$word = $_GET['word'];
+$censured_word = str_replace ( $word, "***", $word);
+// echo $censured_word;
 ?>
 <h1> Ciao <?php echo $word ?></h1>
+<h1> Ciao <?php echo $censured_word ?></h1>
