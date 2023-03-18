bff8784fa9015e770be443959ba7ad283fc3f448
francescogentile25 <francescogentile250893@gmail.com>
2023-03-18 23:35:33 +0100

    REFACTORING

--- form.php
+++ form.php
@@ -1,7 +1,21 @@
 <?php
 $word = $_GET['word'];
-$censured_word = str_replace ( $word, "***", $word);
-// echo $censured_word;
+$text = $_GET['text'];
+$censured_word = str_replace ( $word, "***", $text);
+/*  str_replace(porzioneDaModificare, conCosa, stringa) - 
+    sostituisce una porzione di una stringa con un nuovo valore*/
+//  echo $censured_word;
+var_dump($censured_word);
+var_dump($word); // simile al consol.log di JS
+
 ?>
-<h1> Ciao <?php echo $word ?></h1>
-<h1> Ciao <?php echo $censured_word ?></h1>
+
+<p>  <?php echo $text; ?> <?php echo strlen ($text); ?></p>
+<h1>
+    <?php echo $text. ' la lunghezza è '.strlen($text) ?>
+    <!-- posso sia scrivere una stringa o lasciare vuoto così da concatenare solo le due variabili php -->
+</h1>
+<p>  <?php echo $censured_word; ?></p>
+<h1>
+    <?php echo $censured_word. ' la lunghezza è ' .strlen($censured_word) ?>
+</h1>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <form action="form.php" method="GET">
-    <input type="text" name="word" placeholder="inserisci parola">
+<!-- action inserisco il nome del file al quale devo richiedere o mandare i dati tramite GET o POST  -->
+<textarea name="text"  cols="30" rows="10" placeholder="inserisci il testo"></textarea>
+    <input type="text" name="word" placeholder="inserisci parola da censurare"> 
+    <!-- name serve per mappare i dati che si mandano al server e li recuero poi nel 2 file -->
     <button type="submit">Invia</button>
 </form>
